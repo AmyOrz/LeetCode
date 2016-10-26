40a1383ff8bc1d86e8367535a4fed712f6d83f80
AmyOrz <340606700@qq.com>
2016-10-26 20:35:57 +0800

    linked list

---
+++ database/LinkedList.js
@@ -0,0 +1,93 @@
+var LinkNode = (function () {
+    function LinkNode(value, next) {
+        this.value = value;
+        this.next = next;
+    }
+    return LinkNode;
+}());
+var LinkList = (function () {
+    function LinkList() {
+        this.listArr = [];
+    }
+    LinkList.prototype.getHead = function () {
+        return this.head;
+    };
+    LinkList.prototype.getArrayData = function () {
+        var current = this.head;
+        while (current.next) {
+            this.listArr.push(current.value);
+            current = current.next;
+        }
+        this.listArr.push(current.value);
+        return this.listArr;
+    };
+    LinkList.prototype.append = function (val) {
+        var node = new LinkNode(val, null);
+        if (this.head == void 0) {
+            this.head = node;
+        }
+        else {
+            var current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+    };
+    LinkList.prototype.empty = function () {
+        this.head = { value: null, next: null };
+    };
+    LinkList.prototype.insertAfter = function (target, val) {
+        this._isExistPosition(target);
+        var node = new LinkNode(val, null);
+        var index = 0;
+        var current = this.head;
+        if (target == 0) {
+            node.next = this.head;
+            this.head = node;
+        }
+        else {
+            while (current.next) {
+                current = current.next;
+                if (++index == target) {
+                    node.next = current.next;
+                    current.next = node;
+                    break;
+                }
+            }
+        }
+    };
+    LinkList.prototype.removeAt = function (target) {
+        this._isExistPosition(target);
+        var index = 0;
+        var current = this.head;
+        if (target == 0) {
+            this.head = current.next;
+        }
+        else {
+            while (current.next) {
+                if (++index == target) {
+                    current.next = current.next.next;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    };
+    LinkList.prototype._isExistPosition = function (position) {
+        if (position < 0 || position > this.listArr.length)
+            return -1;
+    };
+    return LinkList;
+}());
+var link = new LinkList();
+link.append(1);
+link.append(11);
+link.append(12);
+link.append(13);
+link.append(10);
+link.append(7);
+link.insertAfter(3, 123);
+link.removeAt(3);
+console.log(link.getArrayData());
+//# sourceMappingURL=LinkedList.js.map
---
+++ database/LinkedList.ts
@@ -0,0 +1,92 @@
+interface Link{
+    value:number,
+    next:Link;
+}
+class LinkNode implements Link{
+    constructor(public value:number,public next:Link){}
+}
+class LinkList{
+    private head:Link;
+    private listArr:number[] = [];
+    getHead():Link{
+        return this.head;
+    }
+    getArrayData():number[]{
+        let current:Link = this.head;
+        while(current.next){
+            this.listArr.push(current.value);
+            current = current.next;
+        }
+        this.listArr.push(current.value);
+        return this.listArr;
+    }
+    append(val:number):void{
+        let node:LinkNode = new LinkNode(val,null);
+        if(this.head == void 0){
+            this.head = node;
+        }else{
+            let current:Link = this.head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+    }
+    empty():void{
+        this.head = {value:null,next:null};
+    }
+    insertAfter(target:number,val:number):void{
+        this._isExistPosition(target);
+
+        let node:LinkNode = new LinkNode(val,null);
+        let index:number = 0;
+        let current:Link = this.head;
+        if(target == 0){
+            node.next = this.head;
+            this.head = node;
+        }else{
+            while(current.next){
+                current = current.next;
+                if(++index == target){
+                    node.next = current.next;
+                    current.next = node;
+                    break;
+                }
+            }
+        }
+    }
+    removeAt(target:number):void{
+        this._isExistPosition(target);
+
+        let index:number = 0;
+        let current:Link = this.head;
+        if(target == 0){
+            this.head = current.next;
+        }else{
+            while(current.next){
+                if(++index == target){
+                    current.next = current.next.next;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    }
+    private _isExistPosition(position:number){
+        if(position < 0 || position >this.listArr.length)
+            return -1;
+    }
+}
+let link:LinkList = new LinkList();
+link.append(1);
+link.append(11);
+link.append(12);
+link.append(13);
+link.append(10);
+link.append(7);
+link.insertAfter(3,123);
+link.removeAt(3);
+
+console.log(link.getArrayData());
+
+
