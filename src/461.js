9dfdea66ff8bc566f971d71594397ef63563ec53
AmyOrz <340606700@qq.com>
2017-01-13 20:04:48 +0800

    461

---
+++ src/461.js
@@ -0,0 +1,5 @@
+var hammingDistance = function (x, y) {
+    if (x < 0 || y < 0 || x > Number.MAX_VALUE || y > Number.MAX_VALUE)
+        return -1;
+};
+//# sourceMappingURL=461.js.map
---
+++ src/461.ts
@@ -0,0 +1,6 @@
+let hammingDistance = (x:number, y:number):number=>{
+    if(x<0 || y<0 || x>Number.MAX_VALUE || y>Number.MAX_VALUE)
+        return -1;
+
+
+};
