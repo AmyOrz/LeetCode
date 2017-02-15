1039d6128cc24c737b78b95e43be22660cf843ea
AmyOrz <340606700@qq.com>
2017-02-15 20:20:23 +0800

    463 leet code

--- src/463.js
+++ src/463.js
@@ -1,1 +1,16 @@
+var islandPerimeter = function (grid) {
+    var island = 0, neighbours = 0;
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] == 1) {
+                island++;
+                if (i < grid.length - 1 && grid[i + 1][j] == 1)
+                    neighbours++;
+                if (j < grid[i].length - 1 && grid[i][j + 1] == 1)
+                    neighbours++;
+            }
+        }
+    }
+    return island * 4 - neighbours * 2;
+};
 //# sourceMappingURL=463.js.map
