bd19ceaff592eb24e67622261f612a01ebb68c2d
AmyOrz <340606700@qq.com>
2017-02-27 21:23:46 +0800

    504

---
+++ src/504.js
@@ -0,0 +1,5 @@
+var convertToBase7 = function (num) {
+    return num.toString(7);
+};
+convertToBase7(100);
+//# sourceMappingURL=504.js.map
---
+++ src/504.ts
@@ -0,0 +1,4 @@
+var convertToBase7 = function(num:number):string{
+    return num.toString(7);
+};
+convertToBase7(100);
