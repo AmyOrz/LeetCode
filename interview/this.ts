89eae88e0d63ca695f64b99bdc6b165d52e6b694
AmyOrz <340606700@qq.com>
2017-04-22 17:30:35 +0800

    learnThis

--- interview/this.ts
+++ interview/this.ts
@@ -1,3 +1,4 @@
+//输出100和1
 var length = 100;
 function getLen(){
     console.log(this.length);
@@ -5,11 +6,15 @@ function getLen(){
 var obj = {
     length:5,
     method:function (fn) {
-        fn();
+        fn();                //内部函数在父函数中被调用时，this都执行global
         arguments[0]();
+    },
+    getLen2:function(){
+        console.log(this.length)
     }
 };
 obj.method(getLen);
+obj.method(obj.getLen2);
 
 
 function fck(){
@@ -28,3 +33,21 @@ function fck(){
 var a = new fck();
 console.log(a.a);
 console.log(a.b);
+
+//这里call使用obj2当做this执行func
+var obj2 = {};
+var func = function(a,b){
+    this.foo = a;    //obj2.foo = a;
+    this.bar = b;    //obj2.bar = b;
+    console.log(this);    //console.log(obj2);
+};
+func.call(obj2,123,321);
+console.log(obj2);
+
+//这里fck1中this指向global，this.hehe则为全局属性，可用global.hehe访问
+function fck1(){
+    console.log(this); //global
+    this.hehe = 213;
+}
+fck1();
+console.log(this.hehe)   //213
