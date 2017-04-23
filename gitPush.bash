b04201ce4cbceb52cb9c81cde3f638a72ad67b8a
AmyOrz <340606700@qq.com>
2017-04-23 18:42:48 +0800

    change the bash gitpush.bash

--- gitPush.bash
+++ gitPush.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-read -p "please input your message for git push : " message1 message2 message3 message4 message5 message6 message7
+read -p "please input 8 messages for git push : " message0 message1 message2 message3 message4 message5 message6 message7
 git add --all
-echo $message1 $message2 $message3 $message4 $message5 $message6 $message7
-git commit -m "$message1 $message2 $message3 $message4 $message5 $message6 $message7"
+git commit -m "$message0 $message1 $message2 $message3 $message4 $message5 $message6 $message7"
 git push origin master
