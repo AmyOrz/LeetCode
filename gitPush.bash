4bb000e2d6e43c34bfa60147bda64b8e8033c1e8
AmyOrz <340606700@qq.com>
2017-08-08 01:11:06 +0800

    ok

--- gitPush.bash
+++ gitPush.bash
@@ -2,7 +2,7 @@
 read -p "please input messages for git push : " message
 git add --all
 git commit -m "$message"
-git fetch qwdq
-git rebaseqwdqd qwdq
+git fetch
+git rebase
 git push origin master
 
