b1789e1e66d66151990ca79cbe2f6c8b0b9d1a8a
yanghao <yanghao@dragonest.com>
2020-07-16 16:30:46 +0800

    remove node_modules

---
+++ config-overrides.js
@@ -0,0 +1,91 @@
+/* config-overrides.js */
+const path = require('path')
+const paths = require('react-scripts/config/paths')
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
+
+const {
+  override,
+  addWebpackAlias,
+  addBabelPlugin,
+  addWebpackPlugin,
+} = require('customize-cra')
+const { getThemeVariables } = require('antd/dist/theme')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const REACT_APP_ENV = process.env.REACT_APP_ENV
+//  env 是 process.env.NODE_ENV  react-app-rewired build 时  env==='production'
+let cdnPublicPath = ''
+let ossProject = '//static.dragonest.com/react-gm-front'
+if (process.env.REACT_APP_ENV === 'qa') {
+  cdnPublicPath = `${ossProject}/qa/`
+} else if (process.env.REACT_APP_ENV === 'prod') {
+  cdnPublicPath = `${ossProject}/prod/`
+} else if (process.env.REACT_APP_ENV === 'dev') {
+  cdnPublicPath = `${ossProject}/dev/`
+} else if (process.env.REACT_APP_ENV === 'pre') {
+  cdnPublicPath = `${ossProject}/pre/`
+}
+const path_config = () => (config, env) => {
+  // 非打包环境下
+  if (config.mode !== 'production') {
+    return config
+  }
+  config.devtool =
+    config.mode === 'development' ? 'cheap-module-source-map' : false
+  // 修改path目录
+  paths.appBuild = path.join(path.dirname(paths.appBuild), 'dist')
+  config.output.path = path.join(path.dirname(config.output.path), 'dist')
+  config.output.publicPath = cdnPublicPath
+  return config
+}
+
+const alter_config = () => (config, env) => {
+  const oneOf_loc = config.module.rules.findIndex(n => n.oneOf)
+  config.module.rules[oneOf_loc].oneOf = [
+    //例如要增加处理less的配置
+    {
+      test: /\.less$/,
+      use: [
+        require.resolve('style-loader'),
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            importLoaders: 1,
+          },
+        },
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              // 如果使用less-loader@5，请移除 lessOptions 这一级直接配置选项。
+              modifyVars: getThemeVariables({
+                dark: false, // 开启暗黑模式
+                compact: false, // 开启紧凑模式
+              }),
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+    },
+    ...config.module.rules[oneOf_loc].oneOf,
+  ]
+  return config
+}
+
+module.exports = override(
+  addBabelPlugin(['@babel/plugin-proposal-decorators', { legacy: true }]),
+  addWebpackPlugin(new AntdDayjsWebpackPlugin()),
+  addWebpackAlias({
+    //增加路径别名的处理
+    '@': path.resolve(__dirname, './src'),
+    '@assets': path.resolve(__dirname, './src/assets'),
+  }),
+  alter_config(),
+  path_config(),
+  addWebpackPlugin(
+    new MonacoWebpackPlugin({
+      languages: ['json','javascript'],
+      features: ['snippets', 'suggest'],
+    }),
+  )
+)
--- src/test/leetcode.test.ts
+++ src/test/leetcode.test.ts
@@ -1,20 +1,173 @@
-describe("finish leet code", () => {
-  const numIdenticalPairs = (nums: number[]) => {
-    let result: number = 0;
+describe('leet code', () => {
+  test('1512', () => {
+    const numIdenticalPairs = (nums: number[]) => {
+      let result: number = 0
 
-    nums.forEach((item, i) => {
-      nums.slice(i + 1).forEach((itemChild) => {
-        if (itemChild == item) {
-          result++;
+      nums.forEach((item, i) => {
+        nums.slice(i + 1).forEach(itemChild => {
+          if (itemChild == item) {
+            result++
+          }
+        })
+      })
+
+      return result
+    }
+
+    expect(numIdenticalPairs([1, 2, 3, 1, 1, 3])).toBe(4)
+    expect(numIdenticalPairs([1, 1, 1, 1])).toBe(6)
+    expect(numIdenticalPairs([1, 2, 3])).toBe(0)
+  })
+
+  test('1431', () => {
+    var kidsWithCandies = function (candies: number[], extraCandies: number) {
+      let max = candies.reduce((result, item) => {
+        if (result < item) {
+          result = item
         }
-      });
-    });
+        return result
+      }, 0)
 
-    return result;
-  };
-  it("1512", () => {
-    expect(numIdenticalPairs([1, 2, 3, 1, 1, 3])).toBe(4);
-    expect(numIdenticalPairs([1, 1, 1, 1])).toBe(6);
-    expect(numIdenticalPairs([1, 2, 3])).toBe(0);
-  });
-});
+      return candies.map(item => {
+        let result = false
+        item + extraCandies >= max ? (result = true) : (result = false)
+
+        return result
+      })
+    }
+
+    expect(kidsWithCandies([2, 3, 5, 1, 3], 3)).toEqual([
+      true,
+      true,
+      true,
+      false,
+      true,
+    ])
+    expect(kidsWithCandies([4, 2, 1, 1, 2], 1)).toEqual([
+      true,
+      false,
+      false,
+      false,
+      false,
+    ])
+    expect(kidsWithCandies([12, 1, 12], 10)).toEqual([true, false, true])
+  })
+
+  test('1470', () => {
+    var testFunc = function (nums: number[], n: number) {
+      let xArray = nums.slice(0, n)
+      let yArray = nums.slice(n)
+      let resultArray = []
+
+      for (let i = 0; i < n; i++) {
+        resultArray.push(xArray[i])
+        resultArray.push(yArray[i])
+      }
+
+      return resultArray
+    }
+
+    expect(testFunc([2, 5, 1, 3, 4, 7], 3)).toEqual([2, 3, 5, 4, 1, 7])
+    expect(testFunc([1, 2, 3, 4, 4, 3, 2, 1], 4)).toEqual([
+      1,
+      4,
+      2,
+      3,
+      3,
+      2,
+      4,
+      1,
+    ])
+    expect(testFunc([1, 1, 2, 2], 2)).toEqual([1, 2, 1, 2])
+  })
+
+  test('1486', () => {
+    var testFunc = function (n: number, start: number) {
+      let array = []
+      let result = 0
+
+      for (let index = 0; index < n; index++) {
+        array.push(start + index * 2)
+      }
+      return array.reduce((result, item) => {
+        return item ^ result
+      }, result)
+    }
+
+    expect(testFunc(5, 0)).toEqual(8)
+    expect(testFunc(4, 3)).toEqual(8)
+    expect(testFunc(1, 7)).toEqual(7)
+    expect(testFunc(10, 5)).toEqual(2)
+  })
+
+  test('offer 58', () => {
+    var testFunc = function (s: string, n: number) {
+      let target = s.slice(0, n)
+      let source = s.slice(n)
+
+      return source + target
+    }
+
+    expect(testFunc('abcdefg', 2)).toEqual('cdefgab')
+    expect(testFunc('lrloseumgh', 6)).toEqual('umghlrlose')
+  })
+
+  test('1299', () => {
+    var testFunc = function (nums: number[]) {
+      return nums.map((item, i) => {
+        let tempArray = nums.slice(i + 1)
+
+        if (tempArray.length == 0) {
+          return -1
+        } else {
+          let max = tempArray.reduce((max, value) => {
+            if (max < value) {
+              max = value
+            }
+            return max
+          }, 0)
+          return max
+        }
+      })
+    }
+
+    expect(testFunc([17, 18, 5, 4, 6, 1])).toEqual([18, 6, 6, 6, 1, -1])
+  })
+
+  test('665', () => {
+    var testFunc = function (nums: number[]) {
+      let firstArr = [...nums]
+      let secondArr = [...nums]
+      let firstResult = true
+      let secondResult = true
+
+      for (let index = 0; index < nums.length - 1; index++) {
+        if (nums[index] > nums[index + 1]) {
+          firstArr[index] = firstArr[index + 1]
+          secondArr[index + 1] = secondArr[index]
+
+          break
+        }
+      }
+      console.log(firstArr, secondArr)
+
+      for (let index = 0; index < nums.length - 1; index++) {
+        if (firstArr[index] > firstArr[index + 1]) {
+          firstResult = false
+        }
+        if (secondArr[index] > secondArr[index + 1]) {
+          secondResult = false
+        }
+      }
+
+      return firstResult || secondResult
+    }
+
+    expect(testFunc([4, 2, 3])).toEqual(true)
+    expect(testFunc([2, 3, 3, 2, 4])).toEqual(true)
+    expect(testFunc([4, 2, 1])).toEqual(false)
+    expect(testFunc([3, 4, 2, 3])).toEqual(false)
+    expect(testFunc([-1, 4, 2, 3])).toEqual(true)
+    expect(testFunc([1, 2, 4, 1])).toEqual(true)
+  })
+})
